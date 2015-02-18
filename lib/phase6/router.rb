7666ab92ffd460288cdfdc68ef3e26f5394a49e6
Richard Nelson <rnelson@alumni.upenn.edu>
2015-02-18 11:49:21 -0500

    Complete Phase 6 (Routing)

--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -11,6 +11,7 @@ module Phase6
 
     # checks if pattern matches path and method matches request method
     def matches?(req)
+      (http_method == req.request_method.downcase.to_sym) && !!(pattern =~ req.path)
     end
 
     # use pattern to pull out route params (save for later?)
@@ -26,28 +27,39 @@ module Phase6
     attr_reader :routes
 
     def initialize
+      @routes = []
     end
 
     # simply adds a new route to the list of routes
     def add_route(pattern, method, controller_class, action_name)
+      @routes << Route.new(pattern, method, controller_class, action_name)
     end
 
     # evaluate the proc in the context of the instance
     # for syntactic sugar :)
     def draw(&proc)
+      instance_eval(&proc)
     end
 
     # make each of these methods that
     # when called add route
     [:get, :post, :put, :delete].each do |http_method|
+      define_method(http_method) do |pattern, controller_class, action_name|
+        add_route(pattern, http_method, controller_class, action_name)
+      end
     end
 
     # should return the route that matches this request
     def match(req)
+      routes.find { |route| route.matches?(req) }
     end
 
     # either throw 404 or call run on a matched route
     def run(req, res)
+      matched_route = match(req)
+      matched_route.nil? ? res.status = 404 : matched_route.run(req, res)
     end
+    
   end
+
 end
